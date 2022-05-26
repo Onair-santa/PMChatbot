863438b74dba94c4ad099c34363a86166dab8a0a
Anvik <42511409+anvikVRN@users.noreply.github.com>
2022-05-26 11:37:26 +0300

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
     WELCOME_TEXT = "Привет.. <b>{}</b>\n<code>Напиши сообщение..\nОтвечу в ближайшее время</code> 😉"
-    USER_DETAILS = "<b>Сообщение от:</b>\nName: {} {}\nId: {}\nUname: @{}\nScam: {}\nRestricted: {}\nStatus: {}\nDc Id: {}"
+    USER_DETAILS = "<code><b>Отправитель:</b>\nName:</code> {} {}\n<code>Id:</code>{}\n<code>Uname:</code> @{}\n<code>Scam:</code> {}\n<code>Restricted:</code> {}\n<code>Status:</code> {}\n<code>Dc Id:</code> {}"
     PM_TXT_ATT = "<code><b>Сообщение от:</b> {} </code>\n<code><b>Имя:</b></code> {}\n\n{}"
     PM_MED_ATT = "<code><b>Сообщение от:</b> {} </code>\n<code><b>Имя:</b></code> {}"
