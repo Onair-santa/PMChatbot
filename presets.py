49bfa2f23b5014ec6b2cc4bcb0855e3d2bd9b9e6
Onair <42511409+Onair-santa@users.noreply.github.com>
2023-04-25 03:47:22 +0600

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
     WELCOME_TEXT = "Hello... **{}**\n`Type your query here..\nI'll respond to your query as earliest` 😉"
-    USER_DETAILS = "🔹<code>Name:</code> <b>{} {}</b>\n       <code>Id:</code>      {}\n       <code>Uname:</code>@{}"
+    USER_DETAILS = "🔹<code>Name:</code> <b>{} {}</b>\n       <code>Id:</code>      <code>{}</code>\n       <code>Uname:</code>@{}"
     PM_TXT_ATT = "📥 :  <code>{}</code>\n <b>{}</b>\n @{}\n\n {}"
     PM_MED_ATT = "📥 :  <code>{}</code>\n <b>{}</b>\n @{}"
