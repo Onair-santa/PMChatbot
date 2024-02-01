c6259e38e65d05fa55df50ff993debf3b60ddbd7
Onair <42511409+Onair-santa@users.noreply.github.com>
2024-02-02 05:48:34 +0600

    Update pm.py

--- plugins/pm.py
+++ plugins/pm.py
@@ -1,4 +1,3 @@
-
 import os
 
 from pyrogram import Client, filters
@@ -19,8 +18,7 @@ async def pm_text(bot, message):
     reference_id = int(message.chat.id)
     await bot.send_message(
         chat_id=Config.ADMIN,
-        text=Presets.PM_TXT_ATT.format(reference_id, info.first_name, info.username, message.text),
-        parse_mode="html"
+        text=Presets.PM_TXT_ATT.format(reference_id, info.first_name, info.username, message.text)
     )
 
 
@@ -35,8 +33,7 @@ async def pm_media(bot, message):
         chat_id=Config.ADMIN,
         from_chat_id=message.chat.id,
         message_id=message.message_id,
-        caption=Presets.PM_MED_ATT.format(reference_id, info.first_name, info.username),
-        parse_mode="html"
+        caption=Presets.PM_MED_ATT.format(reference_id, info.first_name, info.username)
     )
 
 
@@ -75,6 +72,5 @@ async def replay_media(bot, message):
         await bot.copy_message(
             chat_id=int(reference_id),
             from_chat_id=message.chat.id,
-            message_id=message.message_id,
-            parse_mode="html"
+            message_id=message.message_id
         )
