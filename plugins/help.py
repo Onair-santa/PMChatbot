5d37ed62da7cb009fc5b9f7f2e59ab834118ae3a
Onair <42511409+Onair-santa@users.noreply.github.com>
2024-02-02 05:46:47 +0600

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -10,15 +10,14 @@ else:
     from config import Config
 
 
-@Client.on_message(filters.private & filters.command(['start', 'help']))
+@Client.on_message(filters.private & filters.command(['start']))
 async def help_me(bot, message):
     if message.from_user.id == Config.ADMIN:
         return    
     info = await bot.get_users(user_ids=message.from_user.id)
     await bot.send_message(
         chat_id=message.chat.id,
-        text=Presets.WELCOME_TEXT.format(message.from_user.mention, info.first_name),
-        disable_web_page_preview=True
+        text=Presets.WELCOME_TEXT.format(message.from_user.mention, info.first_name)
     )
     await bot.send_message(
         chat_id=Config.ADMIN,
